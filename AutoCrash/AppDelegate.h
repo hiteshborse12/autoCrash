681d53a1c6359f2d3be72f4ce5dba45c5625f176
HItesh <hitesh.borse@wwindia.com>
2016-04-22 15:16:57 +0530

    Initial Commit

---
+++ AutoCrash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoCrash
+//
+//  Created by HItesh on 4/22/16.
+//  Copyright © 2016 Hitesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoCrash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoCrash
+//
+//  Created by HItesh on 4/22/16.
+//  Copyright © 2016 Hitesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
